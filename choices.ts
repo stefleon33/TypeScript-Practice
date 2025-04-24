cfc760f422a8c4bd44f8adb0171049039304ba25
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-04-24 15:08:47 -0400

    Type aliases types

--- choices.ts
+++ choices.ts
@@ -14,5 +14,21 @@ userRole = Role.Guest;
 
 //More popular alternative to enum is to use literal types (instead of general types). The admin,
 // editor and guest are types here, not strings. Literal types are a type where you set very specific 
-// values as types (strings, numbers, etc). Literals are usually used with tuples.
+// values as types (strings, numbers, etc). Literals are usually used with union types.
 let userRole1: 'admin' | 'editor' | 'guest' = 'admin';
+
+// Type aliases or custom types. Useful when dealing with union types.
+// The type specific word allows us to store a type definition under a cutsom name.
+type Roles = 'admin' | 'editor' | 'guest' | 'reader';
+//Roles can then be used throughout the rest of the code without having to repeat it or worry about changes that were made.
+function access(role: Roles) {
+    //...
+}
+
+//Also useful when dealing with object types because those may also be complex.
+type Userss = {
+    name: string;
+    age: number;
+    role: Roles;
+    permissions: string[];
+};
