562eb849593307961d876e3e01e8672a22330823
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-04-24 15:16:41 -0400

    Function Return Types

---
+++ functions.ts
@@ -0,0 +1,5 @@
+//Types can be added after the parameter list, before the curly brace. 
+// This will be the type for the return value. In most cases, the return type does not need to be defined. 
+function add(a:number, b: number): number{
+    return a + b;
+}
