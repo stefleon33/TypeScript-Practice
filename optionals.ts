7adfeb9aec5ea83f580f0c68c57b3bac21ea8a38
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-04-28 17:54:24 -0400

    optional values

---
+++ optionals.ts
@@ -0,0 +1,13 @@
+//Putting a question mark before the colon tells TS that the parameter is optional (it may be specified but it doesn't have to be).
+function generateError(msg?: string) {
+    throw new Error(msg);
+}
+
+generateError();
+
+//Optional properties can also be setup.
+type User = {
+    name: string;
+    age: number;
+    role?: 'admin' | 'guest'
+};
