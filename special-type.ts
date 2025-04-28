ab594768e700279f2f0d8f2f04dcb88854c2842b
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-04-28 11:36:54 -0400

    null and undefined Special types

---
+++ special-type.ts
@@ -0,0 +1,13 @@
+//special types
+
+let a: null | string;
+//null means that a can be set to null but nothing else. 
+//null is helpful when combined with other types, like union types. 'a' can be set to a string or null for example. 
+//Example of when this would be useful is when the variable has a value but then is cleared. 
+a = 'hi!';
+
+a = null;
+
+
+let b: undefined | string;
+//undefined means the variable can hold a string or the value undefined. 
