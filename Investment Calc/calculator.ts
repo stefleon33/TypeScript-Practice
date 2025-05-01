e9867cc5e20b1e3da72042c361b38139da009793
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-05-01 13:27:09 -0400

    Initial layout for investment calc

---
+++ Investment Calc/calculator.ts
@@ -0,0 +1,16 @@
+// data: 
+// initial amount
+// annual contribution
+// expected return
+// duration
+
+function calculateInvestment(data: InvestmentData): InvestmentResult[] {
+
+} // => result[]
+function printResults(results) {
+    //print (output) the result data
+}
+
+const results = calculateInvestment(...)
+
+print(results);
