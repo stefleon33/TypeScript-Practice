e8a4da83378348b07bb5c4c82cac4aa2a7398d1f
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2025-05-01 13:28:19 -0400

    Type for InvestmentResult

--- Investment Calc/calculator.ts
+++ Investment Calc/calculator.ts
@@ -11,9 +11,17 @@ type InvestmentData = {
     duration: number;
 };
 
+type InvestmentResult = {
+    year: string;
+    totalAmount: number;
+    totalContribution: number;
+    totalInterestEarned: number;
+};
+
 function calculateInvestment(data: InvestmentData): InvestmentResult[] {
 
 } // => result[]
+
 function printResults(results) {
     //print (output) the result data
 }
